74762a948b1add41cee303637701b2fbd5106f90
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2023-09-25 16:13:38 +0100

    Update download-extra-dataset.sh

--- download-extra-dataset.sh
+++ download-extra-dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "Downloading Los Angeles crime data from data.gov"
-curl -o ./dataset/csv/crime_data.csv https://data.lacity.org/api/views/2nrs-mtv8/rows.csv\?accessType\=DOWNLOAD
+curl -s -o ./dataset/csv/crime_data.csv https://data.lacity.org/api/views/2nrs-mtv8/rows.csv\?accessType\=DOWNLOAD
 
 if [ $? -eq 0 ]
 then
