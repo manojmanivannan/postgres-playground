42fabcabd8745c9824ec87141715f2b27551e5f4
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2023-09-25 16:28:58 +0100

    Update init.sql

--- dataset/init.sql
+++ dataset/init.sql
@@ -69,3 +69,42 @@ DELIMITER ','
 CSV HEADER;
 
 -- -----------------------------------------------------------------------
+
+CREATE TABLE IF not EXISTS crime_data
+(
+  dr_no int,
+  date_reported datetime,
+  date_occured datetime,
+  time_occured time,
+  area int,
+  area_name varchar(30),
+  reported_dist_no int,
+  part_1_2 int,
+  crime_code int,
+  crime_description varchar(50),
+  mocodes varchar(60),
+  victim_age int,
+  victim_sex varchar(1),
+  victim_descent varchar(1),
+  premis_code int,
+  premis_description varchar(60)
+  weapon_used_code int,
+  weapon_used_description varchar(50),
+  status_code varchar(2),
+  status_description varchar(20),
+  crime_code_1 int,
+  crime_code_2 int,
+  crime_code_3 int,
+  crime_code_4 int,
+  location varchar(60),
+  cross_street varchar(30),
+  latitude numeric(11,8),
+  longitude numeric(11,8)
+);
+
+copy crime_data
+from '/docker-entrypoint-initdb.d/csv/crime_data.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- -----------------------------------------------------------------------
