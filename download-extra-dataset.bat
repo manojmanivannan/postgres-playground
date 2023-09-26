7ebfa5218868fc75cb4eedcbdb3ca09bf40a57f0
Manoj Manivannan <manojm18@live.in>
2023-09-26 15:59:25 +0100

    ignore download if file exits on windows

--- download-extra-dataset.bat
+++ download-extra-dataset.bat
@@ -1,11 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Windows Command Prompt command to download the file using curl
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://data.lacity.org/api/views/2nrs-mtv8/rows.csv\?accessType\=DOWNLOAD', 'dataset\csv\crime_data.csv')"
-
-if !errorlevel! equ 0 (
-  echo Crime data downloaded successfully.
+if not exist dataset\csv\crime_data.csv (
+	rem Windows Command Prompt command to download the file using curl
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://data.lacity.org/api/views/2nrs-mtv8/rows.csv\?accessType\=DOWNLOAD', 'dataset\csv\crime_data.csv')"
+	if !errorlevel! equ 0 (
+		echo Crime data downloaded successfully.
+	) else (
+		echo Crime data download failed.
+	)
 ) else (
-  echo Crime data download failed.
+	echo "crime_data.csv already exists"
 )
